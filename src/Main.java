d8929fa85955ee5f377ddc3cad39abc075e2ae70
ZorahY <zorah.yavaveva@gmail.com>
2022-12-18 12:40:49 -0500

    Feature has been added but it has some errors that need to be resolved

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
        XX.moon=1;
        XX.sun=1;
        XX.PLanet=2;
-       
+
+       FeatureSUN Xeron=new FeatureSUN();
+       Xeron.Heat="123456";
+       Xeron.size=12345678;
+       Xeron.Temperature="34343";
     }
 }
